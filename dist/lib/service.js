48821cce587cc11658eaa7176a359b2bb127c327
murat26 <74151892+muratyayla26@users.noreply.github.com>
2020-12-24 01:03:58 +0300

    try catch statements added for axios get method in service.js

--- dist/lib/service.js
+++ dist/lib/service.js
@@ -1,9 +1,14 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _axios = require('axios'); var _axios2 = _interopRequireDefault(_axios);
 
 /* get user infos */
- async function getData(id){
-    const {data: users} = await _axios2.default.get(`https://jsonplaceholder.typicode.com/users/${id}`);
-    const {data: posts} = await _axios2.default.get(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
-    return {...users, "posts": posts};
-} exports.default = getData;;
+try {
+     async function getData(id){
+        const {data: users} = await _axios2.default.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+        const {data: posts} = await _axios2.default.get(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
+        return {...users, "posts": posts};
+    } exports.default = getData;;
+} catch(e){
+    console.log(e);
+}
+
 
--- src/lib/service.js
+++ src/lib/service.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 
 /* get user infos */
-export default async function getData(id){
-    const {data: users} = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
-    const {data: posts} = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
-    return {...users, "posts": posts};
-};
+try {
+    export default async function getData(id){
+        const {data: users} = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+        const {data: posts} = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
+        return {...users, "posts": posts};
+    };
+} catch(e){
+    console.log(e);
+}
+
 
